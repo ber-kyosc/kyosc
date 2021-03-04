61d7a2b37b60a3767bb352aca745222c539808b7
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-03-04 15:29:13 +0100

    popup.js with simplequote

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,7 +2,7 @@ const popupBox = document.querySelector('#verif-popup');
 const cross = document.querySelector('#close-cross');
 
 function closePopup() {
-    popupBox.classList.remove("d-flex");
+    popupBox.classList.remove('d-flex');
     popupBox.style.display = 'none';
 }
 
