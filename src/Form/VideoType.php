52ec77ec8172a17d890e843314e131b8042c0a7d
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-04-30 11:10:48 +0200

    Form Type for video

---
+++ src/Form/VideoType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Video;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VideoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Titre de la vidéo'
+                ]
+            ])
+            ->add('link', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'lien youtube de la vidéo (URL)'
+                ]
+            ])
+            ->add('save-video', SubmitType::class, [
+                'label' => 'Partager'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Video::class,
+        ]);
+    }
+}
