702ba8090f20993f843eb34e98bb74b4de9e1f0e
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-05-17 15:59:24 +0200

    error corrected in ajax_comment (prefer-template)

--- assets/js/ajax_comment.js
+++ assets/js/ajax_comment.js
@@ -19,21 +19,21 @@ commentForm.submit((e) => {
             }
             let avatar = "<img src='/uploads/profils/default-profil-picture.png' alt='default profil picture' class='avatar-testimony'>";
             if (data.author.profilPhoto !== null) {
-                avatar = "<img src='http://127.0.0.1:8000/media/cache/avatar_thumb/uploads/profils/"
-                    + data.author.profilPhoto
-                    + "' class='avatar-testimony'>";
+                avatar = `<img src='http://127.0.0.1:8000/media/cache/avatar_thumb/uploads/profils/${
+                    data.author.profilPhoto
+                }' class='avatar-testimony'>`;
             }
-            const html = "<div class='align-self-end text-right position-relative'>"
+            const html = `${"<div class='align-self-end text-right position-relative'>"
                     + "<div class='m-3 bubble bubble-bottom-right'>"
-                        + "<p class='pre-line'>" + data.content + '</p>'
+                        + "<p class='pre-line'>"}${data.content}</p>`
                     + '</div>'
-                    + "<h4 class='mt-4'>"
-                        + "<a href='/profil/" + data.author.id + "' class='w-25 text-center'>"
-                            + avatar
-                        + '</a>'
-                        + pseudoAuthor
-                    + '</h4>'
-                    + "<span class='message-date'>Posté à l'instant"
+                    + '<h4 class=\'mt-4\'>'
+                        + `<a href='/profil/${data.author.id}' class='w-25 text-center'>${
+                            avatar
+                        }</a>${
+                            pseudoAuthor
+                        }</h4>`
+                    + '<span class=\'message-date\'>Posté à l\'instant'
                     + '</span>'
                 + '</div>';
             commentSpace.prepend(html);
