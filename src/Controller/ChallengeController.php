5720f7211704ede90074f403ec47a508d49c2983
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-05-27 09:46:58 +0200

    Accepting and rejecting join request OK !

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -17,6 +17,7 @@ use App\Form\VideoType;
 use App\Repository\CategoryRepository;
 use App\Repository\ChallengeRepository;
 use App\Repository\InvitationRepository;
+use App\Repository\JoinRequestRepository;
 use App\Repository\MessageRepository;
 use App\Repository\RequestRepository;
 use App\Repository\SportRepository;
@@ -302,6 +303,62 @@ class ChallengeController extends AbstractController
 
     /**
      * @Route(
+     *     "/{id}/accepter-demande",
+     *     name="accept-request",
+     *     methods={"POST"},
+     *     requirements={"id"="^\d+$"},
+     * )
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param Challenge $challenge
+     * @param JoinRequestRepository $joinRequestRepository
+     * @return Response
+     */
+    public function acceptRequest(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        Challenge $challenge,
+        JoinRequestRepository $joinRequestRepository
+    ): Response {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $creator = $this->getUser();
+        $joinRequestId = $request->request->get('requestId');
+        $submittedToken = $request->request->get('token');
+        if (
+            $this->isCsrfTokenValid('challenge-accept-request', $submittedToken) &&
+            filter_var($joinRequestId, FILTER_VALIDATE_INT)
+        ) {
+            $joinRequest = $joinRequestRepository->find($joinRequestId);
+            if ($joinRequest && $creator) {
+                $user = $joinRequest->getCreator();
+                /* @phpstan-ignore-next-line */
+                $challenge->addParticipant($user);
+                $joinRequests = $joinRequestRepository->findBy([
+                    'challenge' => $challenge,
+                    /* @phpstan-ignore-next-line */
+                    'creator' => $user,
+                ]);
+                if ($joinRequests) {
+                    foreach ($joinRequests as $request) {
+                        if (!$request->getIsAccepted() & !$request->getIsRejected()) {
+                            $request->setIsAccepted(true)
+                                ->setUpdatedAt(new DateTime());
+                            $entityManager->persist($request);
+                            $entityManager->flush();
+                        }
+                    }
+                }
+                $entityManager->flush();
+                return $this->redirectToRoute('challenge_show', [
+                    'id' => $challenge->getId(),
+                ]);
+            }
+        }
+        return $this->redirectToRoute('challenge_index');
+    }
+
+    /**
+     * @Route(
      *     "/{id}/quitter",
      *     name="leave",
      *     methods={"POST"},
@@ -368,6 +425,46 @@ class ChallengeController extends AbstractController
                     $invitation->setIsRejected(true)
                         ->setUpdatedAt(new DateTime());
                     $entityManager->persist($invitation);
+                    $entityManager->flush();
+                }
+            }
+        }
+
+        return $this->redirectToRoute('challenge_show', [
+            'id' => $challenge->getId(),
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/{id}/refuser-demande",
+     *     name="decline-request",
+     *     methods={"POST"},
+     *     requirements={"id"="^\d+$"},
+     * )
+     * @param EntityManagerInterface $entityManager
+     * @param Challenge $challenge
+     * @param JoinRequestRepository $joinRequestRepository
+     * @return Response
+     */
+    public function declineRequest(
+        EntityManagerInterface $entityManager,
+        Challenge $challenge,
+        JoinRequestRepository $joinRequestRepository
+    ): Response {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $creator = $this->getUser();
+        $requests = $joinRequestRepository->findBy([
+            'challenge' => $challenge,
+            /* @phpstan-ignore-next-line */
+            'requestedUser' => $creator,
+        ]);
+        if ($requests) {
+            foreach ($requests as $request) {
+                if (!$request->getIsAccepted() & !$request->getIsRejected()) {
+                    $request->setIsRejected(true)
+                        ->setUpdatedAt(new DateTime());
+                    $entityManager->persist($request);
                     $entityManager->flush();
                 }
             }
